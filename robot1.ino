ab3c7cdd1cfaed06a689bdb4e2d258620fcb9026
thomasbe329 <thomasbe329@nemi.tskoli.is>
2016-11-24 16:24:39 +0000

    Initial code for the robots

---
+++ robot1.ino
@@ -0,0 +1,42 @@
+#include <Servo.h>
+
+Servo leftServo;
+Servo rightServo;
+
+int roll;
+
+void setup()
+{
+  Serial.begin(9600);
+  leftServo.attach(9);
+  rightServo.attach(10);
+  randomSeed(analogRead(0));
+}
+
+void loop()
+{
+  roll = random(10);
+  if(roll < 2) {
+    //leftServo.write(85);
+    //rightServo.write(85);
+    Serial.println('B');
+    delay(1500);
+  } else if(roll < 6) {
+    roll = random(2);
+    if(roll) {
+      //leftServo.write(85);
+      //rightServo.write(100);
+      Serial.println('L');
+    } else {
+      //leftServo.write(100);
+      //rightServo.write(85);
+      Serial.println('R');
+    }
+    delay(1500);
+  } else {
+    //leftServo.write(100);
+    //leftServo.write(100);
+    Serial.println('F');
+    delay(1500);
+  }
+}
---
+++ robot2.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+
+Servo leftServo;
+Servo rightServo;
+
+char msg = ' ';
+
+void setup()
+{
+  Serial.begin(9600);
+  leftServo.attach(9);
+  rightServo.attach(10);
+}
+
+void loop()
+{
+  while(Serial.available()) {
+    msg=Serial.read();
+    if(msg=='B') {
+      leftServo.write(85);
+      rightServo.write(85);
+    }
+    if(msg=='L') {
+      leftServo.write(85);
+      rightServo.write(100);
+    }
+    if(msg=='R') {
+      leftServo.write(100);
+      rightServo.write(85);
+    }
+    if(msg=='F'){
+      leftServo.write(100);
+      rightServo.write(100);
+    }
+  }
+}
