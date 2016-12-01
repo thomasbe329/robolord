5a00c486a99402a09db641080ca6d3b6c4213b95
thomasbe329 <thomasbe329@nemi.tskoli.is>
2016-12-01 13:38:59 +0000

    Add comments for code

--- robot1.ino
+++ robot1.ino
@@ -1,4 +1,4 @@
-#include <Servo.h>
+#include <Servo.h> // Include a library for the motors
 
 Servo leftServo;
 Servo rightServo;
@@ -6,11 +6,19 @@ Servo rightServo;
 int roll;
 int timedelay;
 
+/* Notes:
+ *  This robot will randomly move forwards, backwards, left or right
+ *  and tell another robot to do the same.
+ */
+
 void setup()
 {
+  // Open serial port 9600
   Serial.begin(9600);
+  // Set right and left Servo to their digital pins 
   rightServo.attach(9);
   leftServo.attach(10);
+  // Make random random
   randomSeed(analogRead(0));
   delay(5000);
 }
@@ -19,30 +27,44 @@ void loop()
 {
   roll = random(10);
   timedelay = random(250, 1000);
+  // If roll is between 0-1 then drive backwards and 
+  // tell robot 2 to drive backwards
   if(roll < 2) {
     Serial.println('B');
     leftServo.write(75);
     rightServo.write(75);
     delay(timedelay);
-  } else if(roll < 6) {
+  } 
+  // If roll is between 2-5 then turn
+  else if(roll < 6) {
     roll = random(2);
+    // If roll is 1 then turn left and 
+    // tell robot 2 to turn left
     if(roll) {
       Serial.println('L');
       leftServo.write(75);
       rightServo.write(110);
       delay(465);
-    } else {
+    } 
+    // If roll is 0 then turn right and 
+    // tell robot 2 to turn right
+    else {
       Serial.println('R');
       leftServo.write(110);
       rightServo.write(75);
       delay(465);
     }
-  } else {
+  }
+  // If roll is between 6-9 then drive forwards and 
+  // tell robot 2 to drive forwards
+  else {
     Serial.println('F');
     leftServo.write(110);
     rightServo.write(110);
     delay(timedelay);
   }
+  // Stop for 0,25 secs and 
+  // tell robot 2 to stop
   Serial.println('S');
   leftServo.write(91);
   rightServo.write(91);
--- robot2.ino
+++ robot2.ino
@@ -1,13 +1,21 @@
-#include <Servo.h>
+#include <Servo.h> // Include a library for the motors
 
 Servo leftServo;
 Servo rightServo;
 
 char msg = ' ';
 
+/* Notes:
+ *  This robot will follow the commands of another robot
+ *  and move forwards, backwards, left or right
+ *  depending on the orders.
+ */
+
 void setup()
 {
+  // Open serial port 9600
   Serial.begin(9600);
+  // Set right and left Servo to their digital pins 
   rightServo.attach(9);
   leftServo.attach(10);
 }
@@ -15,23 +23,29 @@ void setup()
 void loop()
 {
   while(Serial.available()) {
+    // Get message from robot 1
     msg=Serial.read();
+    // Drive backwards if message is B
     if(msg=='B') {
       leftServo.write(75);
       rightServo.write(75);
     }
+    // Turn left if message is L
     if(msg=='L') {
       leftServo.write(75);
       rightServo.write(110);
     }
+    // Turn right if message is R
     if(msg=='R') {
       leftServo.write(110);
       rightServo.write(75);
     }
+    // Drive forwards if message is F
     if(msg=='F') {
       leftServo.write(110);
       rightServo.write(110);
     }
+    // Stop moving if message is S
     if(msg=='S') {
       leftServo.write(91);
       rightServo.write(91);
